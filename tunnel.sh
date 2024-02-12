72403d1dfc0ca0175ccb84b1e4d79344a74efae5
Willy Gardiol <willy@gardiol.org>
2024-02-12 17:03:20 +0100

    Added license and disclaimers

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# By Willy Gardiol, provided under the GPLv3 License. https://www.gnu.org/licenses/gpl-3.0.html
+# Publicly available at: https://github.com/gardiol/ssh-tunnel
+# You can contact me at willy@gardiol.org
 
 if [ "$1" = "" ]
 then
